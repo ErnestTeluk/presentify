1c36ca284dc2060abc30ad8786fb077ad784a1e9
Ernest Teluk <49727996+ErnestTeluk@users.noreply.github.com>
2023-03-15 08:16:26 +0100

    Core package (#4)
    * add package-lock.json to git ignore
    
    * wip
    
    * add splitSlides fn
    
    * add tests for splitSlides
    
    * Revert "add tests for splitSlides"
    
    This reverts commit efe8424da2d9ffc8de09606f050da3788dbe15d6.
    
    * add test to splitSlides

---
+++ packages/presentify/src/lib/splitSlides.ts
@@ -0,0 +1,25 @@
+import { Children, ReactElement, ReactNode } from 'react';
+
+export const splitSlides = ({ children }: { children: ReactNode }) => {
+  const splits: number[] = [];
+  const slides = [];
+  const elements = Children.toArray(children);
+
+  elements.forEach((child, idx) => {
+    const { type } = child as ReactElement;
+    if (type === 'hr') {
+      splits.push(idx);
+    }
+  });
+
+  let previousSplit = 0;
+  splits.forEach(split => {
+    const children = [...elements.slice(previousSplit, split)];
+    slides.push(children);
+    previousSplit = split + 1;
+  });
+  const last = [...elements.slice(previousSplit)];
+  slides.push(last);
+
+  return slides;
+};
